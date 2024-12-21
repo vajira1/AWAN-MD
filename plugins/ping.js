67ddc73988013e631cc25a1b1ce46a36a15928d1
PROxANSAR <panhwarking003@gmail.com>
2024-12-21 20:01:52 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -37,10 +37,10 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '*_Panhwar-MD🪄Pinging..._*' })
+        const message = await conn.sendMessage(from, { text: '*_Aᴡᴀɴ-Mᴅ🪄Pinging..._*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*♻️ Panhwar-MD ping is... : ${ping}ms*`}, { quoted: message })
+        await conn.sendMessage(from, { text: `*♻️ Aᴡᴀɴ-MD ping is... : ${ping}ms*`}, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
