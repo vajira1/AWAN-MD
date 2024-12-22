c2439e8ee23b705db0a957f9f298da50d355e7d2
PROxANSAR <panhwarking003@gmail.com>
2024-12-22 08:46:11 +0500

    Update darama+music.js

--- plugins/darama+music.js
+++ plugins/darama+music.js
@@ -17,7 +17,7 @@ const url = data.url
     
     
 let desc = `
-⫷⦁[ *PANHWAR MD MUSIC DOWNLOADING* ]⦁⫸
+⫷⦁[ *AWAN MD MUSIC DOWNLOADING* ]⦁⫸
 
 🎵 *MUSIC FOUND!* 
 
@@ -29,9 +29,9 @@ let desc = `
 
 🎧 *ENJOY THE MUSIC BROUGHT TO YOU!*
 
-> *PANHWAR MD WHATSAPP BOT* 
+> *AWAN MD WHATSAPP BOT* 
 
-> *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀɴsᴀʀ ᴋɪɴɢ* 
+> *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀᴡᴀɴ ᴢᴀᴅᴀ* 
 `
 
 await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
@@ -43,7 +43,7 @@ let downloadUrl = down.dl_url
 
 //send audio message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀɴsᴀʀ ᴋɪɴɢ*"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀᴡᴀɴ ᴢᴀᴅᴀ*"},{quoted:mek})
 
 }catch(e){
 console.log(e)
@@ -70,7 +70,7 @@ const url = data.url
     
     
 let desc = `
-⫷⦁[ *•PANHWAR-MD VIDEO DOWNLOADING* ]⦁⫸ 
+⫷⦁[ *•AWAN-MD VIDEO DOWNLOADING* ]⦁⫸ 
 
 🎥 *VIDEO FOUND!* 
 
@@ -82,9 +82,9 @@ let desc = `
 
 🎬 *ENJOY THE VIDEO BROUGHT TO YOU!*
 
-> *PANHWAR-MD WHATSAPP BOT* 
+> *AWAN-MD WHATSAPP BOT* 
 
-> *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀɴsᴀʀ ᴋɪɴɢ*
+> *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀᴡᴀɴ ᴢᴀᴅᴀ*
 `
 
 await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
@@ -96,7 +96,7 @@ let downloadUrl = down.dl_url
 
 //send video message
 await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀɴsᴀʀ ᴋɪɴɢ*"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀᴡᴀɴ ᴢᴀᴅᴀ*"},{quoted:mek})
 
 }catch(e){
 console.log(e)
