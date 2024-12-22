5cb2e1d91d0a5184d78408f8dbdf23b27dfddc66
PROxANSAR <panhwarking003@gmail.com>
2024-12-22 09:13:12 +0500

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -43,7 +43,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
  *Enjoy these images! 👾*
 
-> 🌈*PANHWAR MD BOT PLUGINS*🏝️`
+> 🌈*AWAN MD BOT PLUGINS*🏝️`
 }, { quoted: mek });
 }
 
