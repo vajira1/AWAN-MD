d9b9f0f8daaf3dae9438fd8c8d222659c2c62797
PROxANSAR <panhwarking003@gmail.com>
2024-12-22 09:20:52 +0500

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -15,7 +15,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *PANHWAR MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©PANHWAR MD BY Ansar Panhwar*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *AWAN MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©AWAN MD BY AWAN ZADA*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
