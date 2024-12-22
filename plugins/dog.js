b3349e9b78b9f43a9ea297f9edc6c1e99cb16764
PROxANSAR <panhwarking003@gmail.com>
2024-12-22 08:47:31 +0500

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: 'PANHWAR MD DOWNLOAD📂DOG PICS\n\n\n> *BY Ansar Panhwar*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: 'AWAN MD DOWNLOAD📂DOG PICS\n\n\n> *ʙʏ ᴀᴡᴀɴ ᴢᴀᴅᴀ*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error Fetching Dog Image🤕: ${e.message}`);
