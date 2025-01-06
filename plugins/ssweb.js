fe4ebe8ee6f1ea92fe770ef90b2f5601eeda0da4
AWAIS <awaisontop346@gmail.com>
2025-01-06 21:30:36 +0500

    Create ssweb.js

---
+++ plugins/ssweb.js
@@ -0,0 +1,53 @@
+// code by AWAIS MD 
+
+const axios = require("axios");
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+  pattern: "sss",
+  alias: ["ssweb"],
+  react: "💫",
+  desc: "Download screenshot of a given link.",
+  category: "other",
+  use: ".ss <link>",
+  filename: __filename,
+}, 
+async (conn, mek, m, {
+  from, l, quoted, body, isCmd, command, args, q, isGroup, sender, 
+  senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, 
+  groupMetadata, groupName, participants, isItzcp, groupAdmins, 
+  isBotAdmins, isAdmins, reply 
+}) => {
+  if (!q) {
+    return reply("Please provide a URL to capture a screenshot.");
+  }
+
+  try {
+
+    const response = await axios.get(`https://api.davidcyriltech.my.id/ssweb?url=${q}`);
+    const screenshotUrl = response.data.screenshotUrl;
+
+    const imageMessage = {
+      image: { url: screenshotUrl },
+      caption: "*WEB SS DOWNLOADER*\n\n> *© Powered By AWAN MD*",
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363353063923961@newsletter',
+          newsletterName: "AWANMD",
+          serverMessageId: 143,
+        },
+      },
+    };
+
+    await conn.sendMessage(from, imageMessage, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    reply("Failed to capture the screenshot. Please try again.");
+  }
+});
+
+//AWAIS MD
